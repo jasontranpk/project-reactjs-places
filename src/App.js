920778040ae2dae87ee6a64214319e46ef0d5544
Jason Tran <jasontran.pk@gmail.com>
2022-11-06 13:35:11 +0700

    turn all routes to be lazy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 } from 'react-router-dom';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import './App.css';
-import UserPlaces from './places/pages/UserPlaces';
+// import UserPlaces from './places/pages/UserPlaces';
 // import Users from './users/pages/Users';
 // import NewPlace from './places/pages/NewPlace';
 // import UpdatePlace from './places/pages/UpdatePlace';
@@ -17,7 +17,7 @@ import { AuthContext } from './shared/context/auth-context';
 import { useAuth } from './shared/hooks/auth-hook';
 import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
 
-// const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'));
+const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'));
 const Users = React.lazy(() => import('./users/pages/Users'));
 const NewPlace = React.lazy(() => import('./places/pages/NewPlace'));
 const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'));
